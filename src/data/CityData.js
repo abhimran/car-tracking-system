30cb3afe3474bfcce041787506b2046611cbbd11
abhimran <abhimrann@gmail.com>
2022-01-24 23:55:49 +0600

    Created demo city page

---
+++ src/data/CityData.js
@@ -0,0 +1,64 @@
+export const CityData = [
+  {
+    id: '1',
+    name: 'Dhaka',
+    lat: '23.7115253',
+    long: '90.4111451',
+  },
+  {
+    id: '2',
+    name: 'Faridpur',
+    lat: '23.6070822',
+    long: '89.8429406',
+  },
+
+  {
+    id: '3',
+    name: 'Gazipur',
+    lat: '24.0022858',
+    long: '90.4264283',
+  },
+
+  {
+    id: '4',
+    name: 'Gopalganj',
+    lat: '23.0050857',
+    long: '89.8266059',
+  },
+  {
+    id: '5',
+    name: 'Jamalpur',
+    lat: '24.937533',
+    long: '89.937775',
+  },
+  {
+    id: '6',
+    name: 'Kishoreganj',
+    lat: '24.444937',
+    long: '90.776575',
+  },
+  {
+    id: '7',
+    name: 'Madaripur',
+    lat: '23.164102',
+    long: '90.1896805',
+  },
+  {
+    id: '8',
+    name: 'Manikganj',
+    lat: '23.8644',
+    long: '90.0047',
+  },
+  {
+    id: '9',
+    name: 'Munshiganj',
+    lat: '23.5422',
+    long: '90.5305',
+  },
+  {
+    id: '10',
+    name: 'Mymensingh',
+    lat: '24.7471',
+    long: '90.4203',
+  },
+];
--- src/pages/City.js
+++ src/pages/City.js
@@ -1,10 +1,96 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ContentWrapper from '../components/ContentWrapper';
+import * as d3 from 'd3';
+import { CityData } from '../data/CityData';
 
 const City = () => {
+  const [parsed, setParsed] = useState({});
+  const uploadFile = useRef(null);
+  const handleFiles = () => {
+    // Load csv File
+    (async () => {
+      const text = await uploadFile.current.files[0].text();
+      const data = await d3.csvParse(text);
+      setParsed(data);
+    })();
+  };
+
+  console.log(parsed);
   return (
     <ContentWrapper>
-      <div>Hello this is from city!</div>
+      <div>
+        <h5>Import CSV file: </h5>
+        <div>
+          <input
+            ref={uploadFile}
+            onChange={handleFiles}
+            type='file'
+            id='input'
+          />
+        </div>
+        <h5>File Data:</h5>{' '}
+      </div>
+
+      <div className='citis'>
+        <div className='mb-3'>
+          <h4 className='mb-2'>
+            Total citis: {CityData.length ? CityData.length : 0}{' '}
+          </h4>
+        </div>
+        <div className='row gx-3 gy-3'>
+          {CityData.map((city) => (
+            <div className='col-md-4' key={city.id}>
+              <div className='single-city'>
+                <div className='city-detail p-3'>
+                  <h5 className='mb-2'>City name: {city.name}</h5>
+                  <p className='text-secondary mb-1'>Latitude: {city.lat}</p>
+                  <p className='text-secondary mb-1'>Longitude: {city.long}</p>
+                  <p className='text-secondary mb-1'>Car: Not assigned yet</p>
+                  <p className='text-secondary mb-1'>
+                    Operator: Not assigned yet
+                  </p>
+                  <p className='text-secondary mb-1'>Operator code:</p>
+                </div>
+
+                <div className='city-actions p-3'>
+                  <button
+                    type='button'
+                    className='btn btn-primary me-2 mb-2'
+                    data-bs-toggle='modal'
+                    data-bs-target={`#update_city_${city.id}`}
+                  >
+                    Update
+                  </button>
+                  <button
+                    type='button'
+                    className='btn btn-danger me-2 mb-2'
+                    // onClick={() => handleDelete(car)}
+                  >
+                    Delete
+                  </button>
+
+                  <button
+                    type='button'
+                    className='btn btn-success me-2 mb-2'
+                    // onClick={() => handleDelete(car)}
+                  >
+                    Assign car
+                  </button>
+
+                  <button
+                    type='button'
+                    className='btn btn-secondary me-2 mb-2'
+                    // onClick={() => handleDelete(car)}
+                  >
+                    Assign operator
+                  </button>
+                </div>
+                {/* <UpdateCarModal item={car} id={`update_car_${car.id}`} /> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </ContentWrapper>
   );
 };
